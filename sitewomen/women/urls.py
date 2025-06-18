85013cb1428b6f9ae35d3e1aa86a0375e6a09f0e
Timur Berezin <corporate.timur01@gmail.com>
2025-06-18 12:46:37 +0400

    Feat: 1.6 Dynamic url, User's converters
    Added new file converters.py for holding url custom url converters
    
    Added new converter: FourDigitYearConverter
    
    Added register_converter calling that register custom conterter for using in urlpattern
    
    Added new url to urlpatterns: 'archive/<year4:year>/' that using new custom converter 'year4'
    
    Renamed categories view to categories_by_id and added dynamic url_path with 'cat_id'
    Added categories_by_slug with dynamic url_path with 'cat_slug'
    
    Added archive view, which is handled by new url to urlpatterns: 'archive/<year4:year>/'

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cats/', views.categories, name='categories')
+    path('cats/<int:cat_id>/', views.categories_by_id, name='categories_by_id'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='categories_by_slug'),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive, name='archive'),
+    path('archive/<year4:year>/', views.archive, name='archive'),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,5 +5,13 @@ def index(request: HttpRequest):
     return HttpResponse('This is the main page.')
 
 
-def categories(request: HttpRequest):
-    return HttpResponse('<h1>This is the categories page.</h1>')
+def categories_by_id(request: HttpRequest, cat_id: int):
+    return HttpResponse(f'<h1>This is the categories by id page with cat_id: {cat_id}.</h1>')
+
+
+def categories_by_slug(request: HttpRequest, cat_slug: str):
+    return HttpResponse(f'<h1>This is the categories by slug page with cat_slug: {cat_slug}.</h1>')
+
+
+def archive(request: HttpRequest, year: int):
+    return HttpResponse(f'<h1>Archive by years: year: {year}</h1>')
