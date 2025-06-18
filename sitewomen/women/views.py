c93447cfafcb0a4bb72ffe87ec2d18ef034728ce
Timur Berezin <corporate.timur01@gmail.com>
2025-06-18 14:28:16 +0400

    Feat: 1.7 GET and POST requests. Request exception handlers
    In settings.py replaced DEBUG to 'False' and ALLOWED_HOSTS to ['localhost']
    
    Added page_not_found view for handle 404 error with user friendly answer. Configured
    this view to handle this error in sitewomen/urls.py using handler404 (system variable) = page_not_found
    
    Added print GET data if exists in categories_by_slug view. Added raise Http404 in archive view
    if requested year more than current year

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
+from django.urls import Resolver404
+from django.utils import timezone
 
 
 def index(request: HttpRequest):
@@ -10,8 +12,16 @@ def categories_by_id(request: HttpRequest, cat_id: int):
 
 
 def categories_by_slug(request: HttpRequest, cat_slug: str):
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f'<h1>This is the categories by slug page with cat_slug: {cat_slug}.</h1>')
 
 
 def archive(request: HttpRequest, year: int):
+    if year > int(timezone.now().year):
+        raise Http404()
     return HttpResponse(f'<h1>Archive by years: year: {year}</h1>')
+
+
+def page_not_found(request: HttpRequest, exception: Resolver404):
+    return HttpResponseNotFound('<h1>Page not found</h1>')
