992badbde474f747585169248066fd4d75b6df48
Timur Berezin <corporate.timur01@gmail.com>
2025-06-20 14:46:14 +0400

    Feat: 2.5 Template's tag "url"
    The new urls to 'add_page', 'contact', 'login' and 'post' was added to urls.py
    
    Replaced name='index' to name='home' in index view path
    
    The menu replaced to list of dictionary with 'title' and 'url_name' for displaying in index.html ('home' in urlpatterns)
    
    New views for display new urls in urlpatterns added in views.py
    
    Updated index.html for displaying new menu

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
-from django.shortcuts import redirect, reverse, render
+from django.shortcuts import render
 from django.urls import Resolver404
-from django.utils import timezone
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -26,21 +29,20 @@ def about(request: HttpRequest):
     return render(request, 'women/about.html')
 
 
-def categories_by_id(request: HttpRequest, cat_id: int):
-    return HttpResponse(f'<h1>This is the categories by id page with cat_id: {cat_id}.</h1>')
+def show_post(request: HttpRequest, post_id: int):
+    return HttpResponse(f'Displaying page with post_id = {post_id}')
 
 
-def categories_by_slug(request: HttpRequest, cat_slug: str):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>This is the categories by slug page with cat_slug: {cat_slug}.</h1>')
+def addpage(request):
+    return HttpResponse('Add article')
 
 
-def archive(request: HttpRequest, year: int):
-    if year > int(timezone.now().year):
-        uri = reverse('categories_by_id', args=('123',))
-        return redirect(uri, permanent=True)
-    return HttpResponse(f'<h1>Archive by years: year: {year}</h1>')
+def contact(request):
+    return HttpResponse('Feedback')
+
+
+def login(request):
+    return HttpResponse('Authorization')
 
 
 def page_not_found(request: HttpRequest, exception: Resolver404):
