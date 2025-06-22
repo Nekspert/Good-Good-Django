a821919523f2e12be5a43f1ff8f456b0fc279b9f
Timur Berezin <corporate.timur01@gmail.com>
2025-06-22 15:42:50 +0400

    Feat: 3.7 Creation a custom model manager
    The women model got new manager - published = PublishedManager()
    That objects manager was avalible - manually define it
    
    The women model got Status class which inherits from models.IntegerChoices
    and define two choices: DRAFT and PUBLISHED with (value, label). This class
    using in PublishedManager for filtering and in is_published field like choices=Status.choices, default=Status.DRAFT

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,29 +1,24 @@
-from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
+from django.shortcuts import get_object_or_404, render
 from django.urls import Resolver404
 
 from .models import Women
+
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
         ]
-data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
-Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
-     'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
-]
 cats_db = [
     {'id': 1, 'name': 'Актрисы'},
     {'id': 2, 'name': 'Певицы'},
     {'id': 3, 'name': 'Спортсменки'},
 ]
 
+
 def index(request: HttpRequest):
-    posts = Women.objects.filter(is_published=True)
+    posts = Women.published.all()
     data = {
         'title': 'Главная страница',
         'menu': menu,
@@ -48,6 +43,7 @@ def show_post(request: HttpRequest, post_slug: str):
     }
     return render(request, 'women/post.html', context=data)
 
+
 def addpage(request: HttpRequest):
     return HttpResponse('Add article')
 
@@ -64,7 +60,7 @@ def show_category(request: HttpRequest, cat_id: int):
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': Women.published.all(),
         'cat_selected': cat_id
     }
 
