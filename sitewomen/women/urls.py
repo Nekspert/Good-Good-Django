ae0c883d4d4609a303ae87f0588a30ea4355f3cf
Timur Berezin <corporate.timur01@gmail.com>
2025-06-22 21:39:41 +0400

    Feat: 4.4 Displaying posts by categories
    The get_categories simple_tag deleted - no using. show_categories - inclusion_tag
    gets cats from Category.objects.all() and not views.cats_db. The 'views.cats_db' deleted
    
    The category path replaced from <int:cat_id> to <slug:cut_slug> and show_category view
    using cat_slug that gets category by using get_object_or_404. Posts get from
    Women published manager and filter by cat_id
    
    The Category model got get_absolute_url for creating valid url for view
    
    base.html, index.html and list_categories.html updated for corrcetly display categories

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
     path('post/<slug:post_slug>/', views.show_post, name='post'),
-    path('category/<int:cat_id>/', views.show_category, name='category')
+    path('category/<slug:cat_slug>/', views.show_category, name='category')
 ]
 # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive, name='archive'),
 # path('archive/<year4:year>/', views.archive, name='archive'),
