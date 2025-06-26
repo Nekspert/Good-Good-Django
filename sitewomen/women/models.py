84604612e83125f1d0462bae35093afe52dff022
Timur Berezin <corporate.timur01@gmail.com>
2025-06-26 22:24:18 +0400

    Feat: 6.2 Setting up the display of the list of articles
    The 'verbose_name' kwarg added to Women and Category models for cool displaying
    in django admin-panel
    
    The Category model got inner class 'Meta' with verbose names for cool displaying
    in preview of django admin-panel
    
    The register was replaced from admin.site.register to @admin.register() decorator
    with target model and decorate class 'WomenAdmin' which configure field's displaying,
    linking, ordering, editing and pagination using 'list_per_page'

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -14,13 +14,15 @@ class Women(models.Model):
 
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     slug = models.SlugField(max_length=255, unique=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
-    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='women')
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.DRAFT, verbose_name='Статус')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категории')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='Теги')
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='women',
+                                   verbose_name='Муж')
 
     class Meta:
         verbose_name = 'Famous woman'
@@ -41,8 +43,12 @@ class Women(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
     slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
