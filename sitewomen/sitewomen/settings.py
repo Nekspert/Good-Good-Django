c9eb1c1dfbec598cbe9daaff9d119aa1664d9e44
Timur Berezin <corporate.timur01@gmail.com>
2025-07-12 13:44:33 +0400

    Feat: 9.11 Reset password algorithm realization
    In the urlpatterns variable write four new paths:
    - 'password_reset/' with PasswordResetView.as_view()
    - 'password_reset/done/' with PasswordResetDoneView.as_view()
    - 'password_reset/<uidb64>/<token>/' with PasswordResetConfirmView.as_view()
    - password_reset/complete/' with PasswordResetCompleteView.as_view()
    
    In the settings.py added i18n support by using LANGUAGES, LOCALE_PATHS and LANGUAGE_CODE variables and new 'django.middleware.locale.LocaleMiddleware'
    
    Five new templates added which using PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView and PasswordResetCompleteView
    
    In the login.html added url to reset password - path('password_reset/', PasswordResetView.as_view())

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -144,3 +145,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LANGUAGES = [
+    ('en', 'English'),
+    ('ru', 'Русский'),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
